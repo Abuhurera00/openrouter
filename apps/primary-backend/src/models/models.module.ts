7ecbfdb6f741d18646e28bdf51af60f9bd6e9fa8
Abuhurera00 <hurerag24@gmail.com>
2026-03-19 15:09:25 +0500

    feat: create all the llm models required endpoints

--- apps/primary-backend/src/models/models.module.ts
+++ apps/primary-backend/src/models/models.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ModelsController } from './models.controller';
 import { ModelsService } from './models.service';
+import { DatabaseModule } from '@workspace/database';
+import { AuthCoreModule } from '@/core/auth-core.module';
+import { CommonModule } from '@/common/common.module';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    AuthCoreModule,
+    CommonModule,
+  ],
   controllers: [ModelsController],
   providers: [ModelsService]
 })
-export class ModelsModule {}
+export class ModelsModule { }
