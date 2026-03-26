8a59fd8460f5e58c9457c68c58ab3426faef6aa8
Abuhurera00 <hurerag24@gmail.com>
2026-03-26 11:43:50 +0500

    fix: set cookies for cross site in prod

--- apps/primary-backend/src/auth/auth.controller.ts
+++ apps/primary-backend/src/auth/auth.controller.ts
@@ -29,7 +29,7 @@ export class AuthController {
         res.cookie('auth', token, {
             httpOnly: true,
             secure: isProduction,
-            sameSite: isProduction ? 'strict' : 'lax',
+            sameSite: isProduction ? 'none' : 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000,
         });
 
